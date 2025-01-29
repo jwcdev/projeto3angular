5e4e9b5a4a9dbd40937adcff3a3661654917b758
jose <jose@amigosdedeus.com>
2025-01-29 15:39:36 -0300

    Primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, TodoCardComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todo-list-16';
+}
